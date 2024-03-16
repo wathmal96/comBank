df51fcf93e1f82a0d5989c17e392ca01c346447b
wathmal96 <ranaweera1125vinura@gmail.com>
2024-03-16 19:14:49 +0530

    comBank

---
+++ web/index.js
@@ -0,0 +1,15 @@
+function aboutUsOver(){
+    // document.getElementById("navCard").style.navCards.shows;
+    // document.querySelector('.navCard').removeAttribute('hidden');
+     document.getElementById("navCard").style.visibility="visible";
+    //  document.querySelector('.navCard').classList.add('shows');
+    
+}
+
+function aboutUsOut(){
+    
+    document.getElementById("navCard").style.visibility="hidden";
+}
+
+// function navCardOver()
+
