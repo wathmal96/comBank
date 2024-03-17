12a01fde6fbe55a9c25efa491d9e187d6cecf20a
wathmal96 <ranaweera1125vinura@gmail.com>
2024-03-17 08:10:55 +0530

    second

--- web/index.js
+++ web/index.js
@@ -11,5 +11,40 @@ function aboutUsOut(){
     document.getElementById("navCard").style.visibility="hidden";
 }
 
-// function navCardOver()
 
+document.getElementById("divP4_1").style.visibility="visible";
+
+function divP4_1Main(){
+    document.getElementById("divP4_1").style.visibility="visible";
+    document.getElementById("divP4_2").style.visibility="hidden";
+    // document.getElementById("divP4_3").style.display="none";
+    
+}
+function divP4_2Main(){
+    document.getElementById("divP4_1").style.visibility="hidden";
+    document.getElementById("divP4_2").style.visibility="visible";
+    // document.getElementById("divP4_3").style.display="none";
+    
+}
+
+// divP4Links
+
+document.getElementById("divP4Link1").style.display="block";
+function divP4_1(){
+    document.getElementById("divP4Link1").style.display="block";
+    document.getElementById("divP4Link2").style.display="none";
+    document.getElementById("divP4Link3").style.display="none";
+    
+}
+function divP4_2(){
+    document.getElementById("divP4Link1").style.display="none";
+    document.getElementById("divP4Link2").style.display="block";
+    document.getElementById("divP4Link3").style.display="none";
+    
+}
+function divP4_3(){
+    document.getElementById("divP4Link1").style.display="none";
+    document.getElementById("divP4Link2").style.display="none";
+    document.getElementById("divP4Link3").style.display="block";
+    
+}
